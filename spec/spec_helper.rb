6ab6ed96b2ebcd564898c9a9caccbca95ad65f04
Seiei Higa <hanachin@gmail.com>
2013-08-01 00:26:41 +0900

    Add settings for capybara, capybara-screenshot, poltergeist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 require 'bundler/setup'
 
+require 'capybara/rspec'
+require 'capybara-screenshot'
+require 'capybara-screenshot/rspec'
+require 'capybara/poltergeist'
+
+Capybara.configure do |c|
+  c.run_server = false
+  c.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new app, js_errors: false
+  end
+  c.default_driver = :poltergeist
+  c.save_and_open_page_path = 'tmp'
+end
+
 Dir.glob('spec/support/**/*.rb') {|f| load f, true }
